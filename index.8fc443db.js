3b6ad5633d5ce98b4cdd054892e2c4eef7609e6e
luisQuintero32 <115516045+luisQuintero32@users.noreply.github.com>
2023-04-30 18:14:07 +0000

    Deploying to gh-pages from @ luisQuintero32/goit-js-hw-10@8da49cedd1f789ee6e8b03756de2d01efa86b95c 🚀

---
+++ index.8fc443db.js
@@ -0,0 +1,2 @@
+var loader=document.querySelector(".loader_page"),timerId=null;function hidenItem(){console.log("hello"),timerId=setInterval((function(){loader.classList.add("loadhide")}),3100)}hidenItem();
+//# sourceMappingURL=index.8fc443db.js.map
